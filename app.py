ebe21aaddb7e0181ad9a246d927192dd818a41c9
Rachmad Hartono <59315926+rachmadhartono8@users.noreply.github.com>
2024-01-15 10:25:11 +0700

    redeploy

--- app.py
+++ app.py
@@ -24,13 +24,27 @@ def output():
         ipk_kum = float(request.form['ipk_kum'])
         if ipk_kum >= 3.0:
             prediction = 'Lulus Tepat Waktu'
+            recommendation = 'Good Job, Terus tingkatkan prestasi dan partisipasi dalam kegiatan akademik dan non-akademik.'
         elif ipk_kum <= 2.9:
             prediction = 'Tidak lulus Tepat Waktu'
+            recommendation = 'Tidak apa-apa, jangan putus semangat, Masih ada kesempatan untuk di perbaiki'
         else:
             prediction = 'Unknown'
 
-        return render_template('output.html', nama_mahasiswa=nama_mahasiswa, nim=nim, ipk_kum=ipk_kum, prediction=prediction)
-        
+        # if prediction == 'Lulus Tepat Waktu' and ipk_kum >= correct_threshold:
+        #     score_prediksi = 80
+        # elif prediction == 'Tidak lulus Tepat Waktu' and ipk_kum <= correct_threshold:
+        #     score_prediksi = 80
+        # else:
+        #     score_prediksi = 50
+
+        # score_prediksi=score_prediksi
+
+        return render_template('output.html', nama_mahasiswa=nama_mahasiswa, nim=nim, ipk_kum=ipk_kum, prediction=prediction, recommendation=recommendation)
+
+@app.route('/predictiongraduation')
+def prediction_graduation():
+    return render_template('predictiongraduation.html')
 
 def students_pred(ipk_kum):
     model = pickle.load(open('model.pkl','rb'))
